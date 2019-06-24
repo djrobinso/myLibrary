fd0c6db177c3002b743d982ee49342f3f826bc81
Deyonta <deyontajanardrobinson@gmail.com>
2019-06-24 02:28:35 -0400

    Working on the controller and interface methods

--- My_Library/src/main/java/library/BookController.java
+++ My_Library/src/main/java/library/BookController.java
@@ -3,7 +3,10 @@
  */
 package library;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,16 +14,36 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @author Deyonta
  *
  */
+
+import library.Book;
+import library.LibraryRepository;
+
+
+@Controller
+@RequestMapping(path="/")
 public class BookController {
+	
+
+	@Autowired 
+	private LibraryRepository libraryRepository;
+	
 	
 	//get the info from the user interface and then check it back into the library database
 	//returns the book information with a confirmation message
 	@GetMapping("/CheckIn")
-	public @ResponseBody String proccessBookIn(@RequestParam String scannedBarcode ) {
+	public @ResponseBody String proccessBookIn(@RequestParam String scannedBookBarcode ) {
+		
+		//check book in with the method to the interface
+		Book pulledBookData = libraryRepository.checkBookIn(scannedBookBarcode);
 		
 		return null;
 	}
 	
 	//Add New Book to database
+	
+	
+	//check new book out
+	
+	
 
 }
--- My_Library/src/main/java/library/LibraryRepository.java
+++ My_Library/src/main/java/library/LibraryRepository.java
@@ -3,6 +3,7 @@
  */
 package library;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import library.Book;
@@ -13,5 +14,11 @@ import library.Book;
  *
  */
 public interface LibraryRepository extends CrudRepository<Book, Integer>{
-
+	
+	/**
+	 * Query to check book in
+	 * 
+	**/
+	@Query("SELECT * FROM libraryData WHERE FIND_IN_SET(left(barcode,7), scannedBarcode )> 0;")
+	Book checkBookIn(String scannedBarcode);
 }
